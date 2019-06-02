d5c4d24fc24e63eb124a21e2e60a3b2ae41a89a6
cthager <50278884+cthager@users.noreply.github.com>
2019-06-02 16:46:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import word_picker
 import check_letter
+import update_status
 word_picker() 
 win = false
 lose = false
-while(win = false && lose = false): 
+while not (win or lose):
+  check_letter()
+  win = update_win()
+  lose = update_lose()
   
