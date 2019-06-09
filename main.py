86bdb16ceee6f6aa51da2ccdc6cfd4d6158c612e
cthager <50278884+cthager@users.noreply.github.com>
2019-06-09 16:38:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,102 @@
-import word_picker
-import check_letter
-import update_status
-hangman()
-win = false
-lose = false
-while not (win or lose):
-  correct_guess = check()
-  win = update_win()
-  lose = update_lose()
+import random
+
+def graphics(tries):
+    if tries == 0:
+        print('------')
+        print('|   0 ')
+        print('|  /I\\')
+        print('|  / \\')
+        print('|')
+        print('-----')
+    elif tries == 1:
+        print('------')
+        print('|   0 ')
+        print('|  /I\\')
+        print('|  /')
+        print('|')
+        print('-----')
+    elif tries == 2:
+        print('------')
+        print('|   0 ')
+        print('|  /I\\')
+        print('|')
+        print('|')
+        print('-----')
+    elif tries == 3:
+        print('------')
+        print('|   0 ')
+        print('|  /I')
+        print('|')
+        print('|')
+        print('-----')
+    elif tries == 4:
+        print('------')
+        print('|   0 ')
+        print('|   I')
+        print('|')
+        print('|')
+        print('-----')
+    elif tries == 5:
+        print('------')
+        print('|   0 ')
+        print('|')
+        print('|')
+        print('|')
+        print('-----')
+    else:
+        print('------')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('-----')
+
+def user_input(word):
+    print(word)
+    for l in word:
+        print('_', end=" ")
+    print()
+    letter = input('Enter a letter: ')
+    return letter
+
+def check(word, letter):
+    if letter in word:
+        return True
+    else:
+        return False
+
+def update_lose(tries):
+    if tries == 0:
+        return True
+    else:
+        return False
+
+def update_win(blanks):
+    if blanks == 0:
+        return True
+    else:
+        return False
+
+def init():
+    l = ["giraffe", "python", "zebra", "lion", "squirrel"]
+    for x in range(1):
+        random.randint(1,5)
+    return l[x]
+
+word = init()
+tries = 6
+graphics(tries)
+letter = user_input(word)
+win = False
+lose = False
+while (not win) or (not lose):
+    if check(word, letter):
+        print('correct')
+    else:
+        tries -= 1
+        print('incorrect')
+    win = update_win(word.count('_'))
+    lose = update_lose(tries)
+    graphics(tries)
+    letter = user_input(word)
   
